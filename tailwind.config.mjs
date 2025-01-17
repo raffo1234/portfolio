e85e20b4556ecb5631ee7031e94ab750d37d1f3b
Rafael <rhmcord@gmail.com>
2025-01-17 16:02:04 -0500

    feat: first commit

---
+++ tailwind.config.mjs
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  theme: {
+    fontFamily: {
+      raleway: ["Raleway", "sans-serif"],
+      manrope: ["Manrope", "sans-serif"],
+      lobster: ["Lobster", "sans-serif"],
+      "nixie-one": ["Nixie One", "sans-serif"],
+    },
+    extend: {},
+  },
+  plugins: [],
+};
